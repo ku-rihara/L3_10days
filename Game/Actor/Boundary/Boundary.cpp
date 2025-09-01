77702dcb58e7b452226b7beb3609db86469801c3
OonoYouji <k023g0007@g.neec.ac.jp>
2025-09-01 14:04:49 +0900

    ボーダーのクラスの定義を作成中

--- Game/Actor/Boundary/Boundary.cpp
+++ Game/Actor/Boundary/Boundary.cpp
@@ -1,1 +1,12 @@
 #include "Boundary.h"
+
+Boundary::Boundary() {}
+
+void Boundary::Init() {
+	obj3d_.reset(Object3d::CreateModel("Boundary"));
+	baseTransform_.Init();
+}
+
+void Boundary::Update() {
+
+}
