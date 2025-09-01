46cb2863548d7224336b56c68139fb8695d03f06
kase haruto <k023g0052@g.neec.ac.jp>
2025-09-01 21:48:33 +0900

    座標を書き換えた時に自動で初期座標を更新するようにしました。

--- Game/Scene/GameScene.h
+++ Game/Scene/GameScene.h
@@ -5,37 +5,39 @@
 #include "Actor/Player/Player.h"
 #include "BaseScene.h"
 #include "SkyBox/SkyBox.h"
+#include "Actor/Station/Enemy/EnemyStation.h"
 
 class GameScene : public BaseScene {
 private:
-    ///========================================================
-    /// Private variants
-    ///========================================================
+	///========================================================
+	/// Private variants
+	///========================================================
 
-    std::unique_ptr<SkyBox> skuBox_         = nullptr;
-    std::unique_ptr<Player> player_         = nullptr;
-    std::unique_ptr<GameCamera> gameCamera_ = nullptr;
+	std::unique_ptr<SkyBox> skuBox_ = nullptr;
+	std::unique_ptr<Player> player_ = nullptr;
+	std::unique_ptr<GameCamera> gameCamera_ = nullptr;
+	std::unique_ptr<EnemyStation> enemyStation_ = nullptr;
 
 public:
-    ///========================================================
-    /// Constralt destract
-    ///========================================================
-    GameScene();
-    ~GameScene() override;
+	///========================================================
+	/// Constralt destract
+	///========================================================
+	GameScene();
+	~GameScene() override;
 
-    ///========================================================
-    /// private method
-    ///========================================================
+	///========================================================
+	/// private method
+	///========================================================
 
-    /// 初期化、更新、描画
-    void Init() override;
-    void Update() override;
-    void ModelDraw() override;
-    void SpriteDraw() override;
-    void SkyBoxDraw() override;
-    void DrawShadow() override;
+	/// 初期化、更新、描画
+	void Init() override;
+	void Update() override;
+	void ModelDraw() override;
+	void SpriteDraw() override;
+	void SkyBoxDraw() override;
+	void DrawShadow() override;
 
-    void Debug() override; /// debug
-    void ViewProjectionUpdate() override;
-    void ViewProssess() override;
+	void Debug() override; /// debug
+	void ViewProjectionUpdate() override;
+	void ViewProssess() override;
 };
