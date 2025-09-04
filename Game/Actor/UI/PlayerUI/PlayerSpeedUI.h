15eb6a493b3aed88ee8234dbe23ee6f4108d67eb
OonoYouji <k023g0007@g.neec.ac.jp>
2025-09-05 00:08:35 +0900

    スピードも表示

--- Game/Actor/UI/PlayerUI/PlayerSpeedUI.h
+++ Game/Actor/UI/PlayerUI/PlayerSpeedUI.h
@@ -3,6 +3,7 @@
 #include <memory>
 
 #include "2d/Sprite.h"
+#include "../NumDraw/NumDraw.h"
 
 class PlayerSpeedUI {
 public:
@@ -10,12 +11,13 @@ public:
 	~PlayerSpeedUI() = default;
 
 	void Init();
-	void Update();
+	void Update(class Player* _player);
 	void Draw();
 
 private:
 
 	std::unique_ptr<Sprite> speedUISprite_;
+	std::unique_ptr<NumDraw> speedNumDraw_;
 
 };
 
