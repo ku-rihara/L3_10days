85a846b7a1950ec890dfb4e109182da6594634ac
OonoYouji <k023g0007@g.neec.ac.jp>
2025-09-09 02:43:55 +0900

    テクスチャを正しい位置に配置

--- Game/GameClearActor/GameClearSprite.cpp
+++ Game/GameClearActor/GameClearSprite.cpp
@@ -6,101 +6,84 @@
 GameClearSprite::GameClearSprite() = default;
 GameClearSprite::~GameClearSprite() = default;
 
+
 void GameClearSprite::Init() {
 
 	sprites_.resize(MAX_SPRITE);
 
-	{	/// RESULT_WINDOW
+	float centerX = 640.0f - 200.0f;
+
+	{   /// RESULT_WINDOW
 		auto& sp = sprites_[RESULT_WINDOW];
-		uint32_t textureHandle = TextureManager::GetInstance()->LoadTexture("./resources/Texture/GameResult/ResultWindow.png");
-		sp.reset(Sprite::Create(textureHandle, { 640.0f, 360.0f }, { 1, 1, 1, 1 }));
+		uint32_t texHandle = TextureManager::GetInstance()->LoadTexture("./resources/Texture/GameResult/ResultWindow.png");
+		sp.reset(Sprite::Create(texHandle, { centerX, 360.0f }, { 1, 1, 1, 1 }));
 		sp->anchorPoint_ = { 0.5f, 0.5f };
-		Vector2 texSize = sp->GetTextureSize();
-		Vector2 size = Vector2{ 800.0f, 600.0f };
-		sp->SetScale(size / texSize);
 	}
 
-	{	/// MISSION_RESULT
+	{   /// MISSION_RESULT
 		auto& sp = sprites_[MISSION_RESULT];
-		uint32_t textureHandle = TextureManager::GetInstance()->LoadTexture("./resources/Texture/GameResult/MissionResult.png");
-		sp.reset(Sprite::Create(textureHandle, { 640.0f, 100.0f }, { 1, 1, 1, 1 }));
+		uint32_t texHandle = TextureManager::GetInstance()->LoadTexture("./resources/Texture/GameResult/MissionResult.png");
+		sp.reset(Sprite::Create(texHandle, { centerX, 120.0f }, { 1, 1, 1, 1 }));
 		sp->anchorPoint_ = { 0.5f, 0.5f };
-		Vector2 texSize = sp->GetTextureSize();
-		Vector2 size = Vector2{ 400.0f, 100.0f };
-		sp->SetScale(size / texSize);
 	}
 
-	{	/// TOTAL_TIME
+
+
+	{   /// TOTAL_TIME
 		auto& sp = sprites_[TOTAL_TIME];
-		uint32_t textureHandle = TextureManager::GetInstance()->LoadTexture("./resources/Texture/GameResult/TotalTime.png");
-		sp.reset(Sprite::Create(textureHandle, { 400.0f, 200.0f }, { 1, 1, 1, 1 }));
+		uint32_t texHandle = TextureManager::GetInstance()->LoadTexture("./resources/Texture/GameResult/TotalTime.png");
+		sp.reset(Sprite::Create(texHandle, { 160.0f, 220.0f }, { 1, 1, 1, 1 }));
 		sp->anchorPoint_ = { 0.0f, 0.5f };
-		Vector2 texSize = sp->GetTextureSize();
-		Vector2 size = Vector2{ 200.0f, 50.0f };
-		sp->SetScale(size / texSize);
 	}
 
-	{	/// TIME_BONUS
+	{   /// TIME_BONUS
 		auto& sp = sprites_[TIME_BONUS];
-		uint32_t textureHandle = TextureManager::GetInstance()->LoadTexture("./resources/Texture/GameResult/TimeBonus.png");
-		sp.reset(Sprite::Create(textureHandle, { 400.0f, 260.0f }, { 1, 1, 1, 1 }));
+		uint32_t texHandle = TextureManager::GetInstance()->LoadTexture("./resources/Texture/GameResult/TimeBonus.png");
+		sp.reset(Sprite::Create(texHandle, { 160.0f, 280.0f }, { 1, 1, 1, 1 }));
 		sp->anchorPoint_ = { 0.0f, 0.5f };
-		Vector2 texSize = sp->GetTextureSize();
-		Vector2 size = Vector2{ 200.0f, 50.0f };
-		sp->SetScale(size / texSize);
 	}
 
-	{	/// DESTROY_COUNT
+	{   /// DESTROY_COUNT
 		auto& sp = sprites_[DESTROY_COUNT];
-		uint32_t textureHandle = TextureManager::GetInstance()->LoadTexture("./resources/Texture/GameResult/TotalBreak.png");
-		sp.reset(Sprite::Create(textureHandle, { 400.0f, 320.0f }, { 1, 1, 1, 1 }));
+		uint32_t texHandle = TextureManager::GetInstance()->LoadTexture("./resources/Texture/GameResult/TotalBreak.png");
+		sp.reset(Sprite::Create(texHandle, { 160.0f, 340.0f }, { 1, 1, 1, 1 }));
 		sp->anchorPoint_ = { 0.0f, 0.5f };
-		Vector2 texSize = sp->GetTextureSize();
-		Vector2 size = Vector2{ 200.0f, 50.0f };
-		sp->SetScale(size / texSize);
 	}
 
-	{	/// DESTROY_BONUS
+	{   /// DESTROY_BONUS
 		auto& sp = sprites_[DESTROY_BONUS];
-		uint32_t textureHandle = TextureManager::GetInstance()->LoadTexture("./resources/Texture/GameResult/BreakBonus.png");
-		sp.reset(Sprite::Create(textureHandle, { 400.0f, 380.0f }, { 1, 1, 1, 1 }));
+		uint32_t texHandle = TextureManager::GetInstance()->LoadTexture("./resources/Texture/GameResult/BreakBonus.png");
+		sp.reset(Sprite::Create(texHandle, { 160.0f, 400.0f }, { 1, 1, 1, 1 }));
 		sp->anchorPoint_ = { 0.0f, 0.5f };
-		Vector2 texSize = sp->GetTextureSize();
-		Vector2 size = Vector2{ 200.0f, 50.0f };
-		sp->SetScale(size / texSize);
 	}
 
-	{	/// FINAL_SCORE
-		auto& sp = sprites_[FINAL_SCORE];
-		uint32_t textureHandle = TextureManager::GetInstance()->LoadTexture("./resources/Texture/GameResult/TotalScore.png");
-		sp.reset(Sprite::Create(textureHandle, { 400.0f, 440.0f }, { 1, 1, 1, 1 }));
-		sp->anchorPoint_ = { 0.0f, 0.5f };
-		Vector2 texSize = sp->GetTextureSize();
-		Vector2 size = Vector2{ 200.0f, 50.0f };
-		sp->SetScale(size / texSize);
-	}
-
-	{	/// LINE
+	{   /// LINE
 		auto& sp = sprites_[LINE];
-		uint32_t textureHandle = TextureManager::GetInstance()->LoadTexture("./resources/Texture/GameResult/Line.png");
-		sp.reset(Sprite::Create(textureHandle, { 640.0f, 500.0f }, { 1, 1, 1, 1 }));
+		uint32_t texHandle = TextureManager::GetInstance()->LoadTexture("./resources/Texture/GameResult/Line.png");
+		sp.reset(Sprite::Create(texHandle, { centerX, 460.0f }, { 1, 1, 1, 1 }));
 		sp->anchorPoint_ = { 0.5f, 0.5f };
-		Vector2 texSize = sp->GetTextureSize();
-		Vector2 size = Vector2{ 600.0f, 5.0f };
-		sp->SetScale(size / texSize);
 	}
 
-	{	/// TO_TITLE
+	{   /// FINAL_SCORE
+		auto& sp = sprites_[FINAL_SCORE];
+		uint32_t texHandle = TextureManager::GetInstance()->LoadTexture("./resources/Texture/GameResult/TotalScore.png");
+		sp.reset(Sprite::Create(texHandle, { 160.0f, 520.0f }, { 1, 1, 1, 1 }));
+		sp->anchorPoint_ = { 0.0f, 0.5f };
+	}
+
+
+	{   /// TO_TITLE
 		auto& sp = sprites_[TO_TITLE];
-		uint32_t textureHandle = TextureManager::GetInstance()->LoadTexture("./resources/Texture/GameResult/ReturnTitle.png");
-		sp.reset(Sprite::Create(textureHandle, { 640.0f, 550.0f }, { 1, 1, 1, 1 }));
+		uint32_t texHandle = TextureManager::GetInstance()->LoadTexture("./resources/Texture/GameResult/ReturnTitle.png");
+		sp.reset(Sprite::Create(texHandle, { 980.0f, 600.0f }, { 1, 1, 1, 1 })); // ←右寄せ
 		sp->anchorPoint_ = { 0.5f, 0.5f };
 		Vector2 texSize = sp->GetTextureSize();
-		Vector2 size = Vector2{ 300.0f, 50.0f };
+		Vector2 size = { 300.0f, 50.0f };
 		sp->SetScale(size / texSize);
 	}
-
 }
+
+
 
 void GameClearSprite::Update() {}
 
