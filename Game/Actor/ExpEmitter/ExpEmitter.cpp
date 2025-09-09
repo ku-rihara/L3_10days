ee870ab20940f404510860e765d80c8b89887b13
ku-rihara <k023g0027@g.neec.ac.jp>
2025-09-10 01:31:14 +0900

    exp getinstance

--- Game/Actor/ExpEmitter/ExpEmitter.cpp
+++ Game/Actor/ExpEmitter/ExpEmitter.cpp
@@ -1,6 +1,9 @@
 #include"ExpEmitter.h"
 
-
+ExpEmitter* ExpEmitter::GetInstance() {
+    static ExpEmitter instance;
+    return &instance;
+}
 void ExpEmitter::Init() {
     emitter_[2].reset(ParticleEmitter::CreateParticlePrimitive("Explosion1", PrimitiveType::Box, 500));
     emitter_[0].reset(ParticleEmitter::CreateParticlePrimitive("Explosion2", PrimitiveType::Box, 500));
