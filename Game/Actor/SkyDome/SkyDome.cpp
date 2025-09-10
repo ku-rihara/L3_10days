85f276aef716b03a77b10e4cbd872464db0ef334
OonoYouji <k023g0007@g.neec.ac.jp>
2025-09-10 11:54:44 +0900

    RouteCollectionファイルだけ設定が上書きされていたのでReleaseビルドに失敗していた

--- Game/Actor/SkyDome/SkyDome.cpp
+++ Game/Actor/SkyDome/SkyDome.cpp
@@ -2,6 +2,8 @@
 
 /// engine
 #include "3d/ModelManager.h"
+#include "Frame/Frame.h"
+
 
 void SkyDome::Init() {
 	const std::string instanceName = "skydome.obj";
@@ -24,12 +26,27 @@ void SkyDome::Init() {
 	float scale = 1500.0f * 4.0f;
 	obj3d_->transform_.scale_ = { scale, scale, scale };
 	obj3d_->Update();
+
+	time_ = 0.0f;
 }
 
-void SkyDome::Update() {
-
-}
+void SkyDome::Update() {}
 
 void SkyDome::Draw(const ViewProjection& _vp) {
 	obj3d_->Draw(_vp);
 }
+
+
+BackgroundObj::BackgroundObj() {
+	const std::string instanceName = "skydome.obj";
+	obj3d_ = std::make_unique<Object3d>();
+	ModelManager::GetInstance()->LoadModel(instanceName);
+	obj3d_->transform_.Init();
+	obj3d_->SetModel(instanceName);
+	obj3d_->CreateWVPResource();
+	obj3d_->CreateShadowMap();
+	obj3d_->CreateMaterialResource();
+	obj3d_->SetTexture("SkyDome.png");
+}
+
+BackgroundObj::~BackgroundObj() = default;
